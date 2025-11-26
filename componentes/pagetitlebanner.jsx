4726cde6616ad7449d276033ac7ac81b39612f1d
Abner Soares <abnerprofsse@gmail.com>
2025-11-26 14:52:53 -0300

    Add PageTitleBanner component for displaying titles

---
+++ componentes/pagetitlebanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PageTitleBanner({ title }) { 
+  return (
+    <div className="titulopagina"> 
+      <h2><b>{title}</b></h2>
+    </div>
+  );
+}
+
+export default PageTitleBanner;
